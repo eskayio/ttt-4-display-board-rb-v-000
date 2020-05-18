5dd68608c9878006b3553513a2706ba60a9abec0
Samir Singh <singhstart@gmail.com>
2020-05-18 06:46:18 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 
 def display_board(board)
-  rows =["#{board[0]}  | #{board[1]} | #{board[2]}"],
+  rows =["#{board[0]}  | #{board[1]} | #{board[2]} "],
   ["-----------"],
   ["#{board[3]} | #{board[4]} | #{board[5]}"],
   ["-----------"],
